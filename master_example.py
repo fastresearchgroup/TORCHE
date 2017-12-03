848db11a56c3e2956d2503fb8557273dfb2da9a2
lcarasik <lcar722@gmail.com>
2017-12-02 22:26:48 -0800

    This commit updates the master_example to print out the pressure drop in kPa.

--- master_example.py
+++ master_example.py
@@ -16,10 +16,10 @@ Nu2 = 147.31
 
 import TORCHE
 dP_1 = TORCHE.dP_Zu(1.25,1.25,.5,1940,.0056,10,.0254,22000,'inline')
-print('The Pressure Drop calculated by Zukauskas is',dP_1,'kPa')
+print('The Pressure Drop calculated by Zukauskas is',dP_1/1000,'kPa')
 dP_2 = TORCHE.dP_GG(1.25,1.25,.5,1940,22000,10,'inline')
-print('The Pressure Drop calculated by Gaddis-Gnielinski is',dP_2,'kPa')
-Nu_1=TORCHE.HT_Zu(1.25,1.25,.5,1940,10,1,1,.0254,1.787e-6,'inline',22000)
+print('The Pressure Drop calculated by Gaddis-Gnielinski is',dP_2/1000,'kPa')
+Nu_1 = TORCHE.HT_Zu(1.25,1.25,.5,1940,10,1,1,.0254,1.787e-6,'inline',22000)
 print('The Nusselt Number calculated by Zukauskas is', Nu_1)
-Nu_2=TORCHE.HT_GG(1.25,1.25,.0254,.5,1940,22000,10,'inline',1)
+Nu_2 = TORCHE.HT_GG(1.25,1.25,.0254,.5,1940,22000,10,'inline',1)
 print('The Nusselt Number calculated by Gnielinski is', Nu_2)
