060303abcce3995917fea957f07df2cb219666d2
lcarasik <lane.carasik@gmail.com>
2022-01-04 16:00:21 -0500

    Initial addition of 3stream capability

---
+++ 3stream.py
@@ -0,0 +1,41 @@
+'''
+Author: Lane Carasik
+Purpose: Three-stream heat exchanger temperature distribution (1-D)
+'''
+
+#----------------------------------------------------------------------------------#
+# import modules
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#----------------------------------------------------------------------------------#
+# Options/Inputs
+
+# Solver Settings
+nodes = 100  # Number of nodes for 1-D calculation
+
+# Input Conditions
+# Temperatures
+T_1in = 30 # Inlet Temperature (Stream 1)
+T_2in = 50 # Inlet Temperature (Stream 2)
+T_3in = 80 # Inlet Temperature (Stream 3)
+# Flow rates
+V_flow_1 = 1 # Flow rate (Stream 1) 
+V_flow_2 = 5 # Flow rate (Stream 2) 
+V_flow_3 = 3 # Flow rate (Stream 3) 
+
+# Call Fluid properties
+
+#----------------------------------------------------------------------------------#
+# Basic Global Calulations
+
+# Call mass flow rate calculation 
+# Call area calculation
+# Call Hydraulic diameter calculation
+# Call Reynolds number calculation
+# Call Prandtl number calcation
+# Call Nusselt number calculation
+# Call HTC Calculation
+
