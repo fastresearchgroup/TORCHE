0cefe35831af72fc3eb3eaf26e65cd509cbef96d
lcarasik <lane.carasik@gmail.com>
2022-01-05 10:11:48 -0500

    Added Areas to it

--- 3stream.py
+++ 3stream.py
@@ -11,6 +11,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #----------------------------------------------------------------------------------#
+# Define functions needed
+
+def Prandtl(c_p,mu,k):
+	# Prandtl number calculation
+	Pr = c_p*mu/k
+	
+	return Pr
+    
+def Reynolds(rho,vel,D_h,mu):
+	# Reynolds number calculation
+	Re = rho*vel*D_h/mu
+	
+	return Re
+
+#----------------------------------------------------------------------------------#
 # Options/Inputs
 
 # Solver Settings
@@ -27,15 +42,29 @@ V_flow_2 = 5 # Flow rate (Stream 2)
 V_flow_3 = 3 # Flow rate (Stream 3) 
 
 # Call Fluid properties
+'''
+Currently assuming liquids and not gases.
+'''
+
 
 #----------------------------------------------------------------------------------#
 # Basic Global Calulations
 
 # Call mass flow rate calculation 
+
 # Call area calculation
+A_1 = .010 # Flow area of stream 1
+A_2 = .015 # Flow area of stream 2
+A_3 = .025 # Flow area of stream 3
+
 # Call Hydraulic diameter calculation
+
 # Call Reynolds number calculation
+
 # Call Prandtl number calcation
+Pr_1 = Prandtl(c_p,mu,k)
+Pr_2 = Prandtl(c_p,mu,k)
+Pr_2 = Prandtl(c_p,mu,k)
 # Call Nusselt number calculation
 # Call HTC Calculation
 
